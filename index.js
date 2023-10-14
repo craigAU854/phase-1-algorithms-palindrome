6b5a865a0b435312593301f929997c14f1de378f
Craig Otieno <craigaustin021@gmail.com>
2023-10-14 14:09:18 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //loop through string halfway
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
   Add written explanation of your solution here
+  
+   
 */
 
 // You can run `node index.js` to view these console logs
